4c6a08de7dd92d2048960e508030a58deffab046
SuheL-Roy <60100467+SuheL-Roy@users.noreply.github.com>
2021-06-17 20:44:39 +0600

    2nd

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-console.log("welcome to banglades");
+
+const imageUpload = document.getElementById('imageUploaded');
+
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
+]).then(start)
+
+function start() {
+    document.body.append("Loaded")
+    imageUpload.addEventListener('change' , async () => {
+        const image = await faceapi.bufferToImage(imageUpload.files[0]);
+        document.append(image);
+        const detections = await faceapi.detectAllFaces(image)
+        .withFaceLandmarks().withFaceDescriptors()
+        document.body.append(detections.length);
+    })
+}
