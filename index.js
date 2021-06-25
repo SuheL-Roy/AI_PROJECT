c955895ecb86bcd3b4d4566061fdeb41cf7ef6da
SuheL-Roy <60100467+SuheL-Roy@users.noreply.github.com>
2021-06-25 23:29:42 +0600

    2nd

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
 ]).then(start)
 
-function start() {
+ async function start() {
+   const container = document.createElement('div')
+   container.style.position= 'relative'
+   document.body.append(container)
     document.body.append("Loaded")
     imageUpload.addEventListener('change' , async () => {
         const image = await faceapi.bufferToImage(imageUpload.files[0]);
-        document.append(image);
+        document.body.append(image);
+        const canvas = faceapi.createCanvasFromMedia(image)
+        document.body.append(canvas);
         const detections = await faceapi.detectAllFaces(image)
         .withFaceLandmarks().withFaceDescriptors()
         document.body.append(detections.length);
