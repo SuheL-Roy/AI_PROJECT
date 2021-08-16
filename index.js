4db62ec8500c8f6e809f8b8370ad8fdfaa2efa61
SuheL-Roy <60100467+SuheL-Roy@users.noreply.github.com>
2021-08-16 12:42:32 +0600

    last

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-
-const imageUpload = document.getElementById('imageUploaded');
+const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
@@ -8,18 +6,47 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
 ]).then(start)
 
- async function start() {
-   const container = document.createElement('div')
-   container.style.position= 'relative'
-   document.body.append(container)
-    document.body.append("Loaded")
-    imageUpload.addEventListener('change' , async () => {
-        const image = await faceapi.bufferToImage(imageUpload.files[0]);
-        document.body.append(image);
-        const canvas = faceapi.createCanvasFromMedia(image)
-        document.body.append(canvas);
-        const detections = await faceapi.detectAllFaces(image)
-        .withFaceLandmarks().withFaceDescriptors()
-        document.body.append(detections.length);
+async function start() {
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
     })
+  })
+}
+
+function loadLabeledImages() {
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark','Suhel']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`./labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
 }
